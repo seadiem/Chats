0637dac91bdf4bb5ae2c5f94816983155bdfe24d
Alex Sed <se-a@mail.ru>
2019-09-19 18:19:43 +0300

    Do

--- Sources/ChatClient/Configs/DogOne.swift
+++ Sources/ChatClient/Configs/DogOne.swift
@@ -4,27 +4,27 @@ import Foundation
 public struct ChatClientData {
     
     let name: String
-    let leash: Leash
+//    let leash: Leash
 //    let handler: (Data) -> Void
 //    let queue: DispatchQueue
     
     public init(name: String) {
         self.name = name
-        self.leash = try! Leash()
+ //       self.leash = try! Leash()
 //        self.handler = handler
 //        self.queue = DispatchQueue(label: "socket listener")
     }
     
     func send(data: Data) {
-        do { try leash.clientSocket.write(from: data) }
-        catch let error { print(error) }
+//        do { try leash.clientSocket.write(from: data) }
+//        catch let error { print(error) }
     }
     
     func listen() {
         while true {
             do {
                 var data = Data()
-                _ = try self.leash.clientSocket.read(into: &data)
+//                _ = try self.leash.clientSocket.read(into: &data)
 //                self.handler(data)
             } catch let error {
                 print(error)
