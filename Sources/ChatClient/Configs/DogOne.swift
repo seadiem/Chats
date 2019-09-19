270500ac16e2f56dd468b6f88739f82df4a43daa
Alex Sed <se-a@mail.ru>
2019-09-19 18:24:01 +0300

    Do

--- Sources/ChatClient/Configs/DogOne.swift
+++ Sources/ChatClient/Configs/DogOne.swift
@@ -32,3 +32,32 @@ public struct ChatClientData {
         }
     }    
 }
+
+//import ChatYard
+//import Foundation
+//
+//print("Представьтесь:")
+//let message = readLine()
+//var name = message ?? "User"
+//
+//do {
+//    let dog = try Leash()
+//    
+//    let queue = DispatchQueue(label: "keyboard listen")
+//    queue.async {
+//        keyinput: while true {
+//            let message = readLine()
+//            guard let line = message else { continue keyinput }
+//            do { try dog.clientSocket.write(from: name + ": " + line) }
+//            catch let error { print(error) }
+//        }
+//    }
+//    
+//    while true {
+//        let incom = try dog.clientSocket.readString()
+//        print("from sever: \(incom ?? "")")
+//    }
+//    
+//} catch let error {
+//    print(error)
+//}
